952127b0780c9cc23f4245dde9a8d847b4d89a1b
MedChahineHouidhek <chahinehouidhek2@gmail.com>
2023-12-20 19:07:49 -0500

    ajout de design

--- Views/login.php
+++ Views/login.php
@@ -4,24 +4,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
 
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Login</title>
 </head>
 
 <body>
 
-    <h1>Login</h1>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h1 class="card-title text-center">Login</h1>
+                        <form action="index.php?action=login" method="post">
+                            <div class="form-group">
+                                <label for="username">Username:</label>
+                                <input type="text" class="form-control" id="username" name="username" required>
+                            </div>
 
-    <form action="index.php?action=login" method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required><br>
+                            <div class="form-group">
+                                <label for="password">Password:</label>
+                                <input type="password" class="form-control" id="password" name="password" required>
+                            </div>
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br>
+                            <button type="submit" class="btn btn-primary btn-block">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-        <button type="submit">Login</button>
-    </form>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
 </body>
 
--- Views/register.php
+++ Views/register.php
@@ -4,35 +4,58 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
     <title>Register</title>
 </head>
 
 <body>
-    <h1>Register</h1>
 
-    <form action="index.php?action=register" method="post">
-        <label for="email">Email:</label>
-        <input type="email" name="email" required><br>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h1 class="card-title text-center">Register</h1>
+                        <form action="index.php?action=register" method="post">
+                            <div class="form-group">
+                                <label for="email">Email:</label>
+                                <input type="email" class="form-control" name="email" required>
+                            </div>
 
-        <!-- Remove the input for token to be generated automatically -->
+                            <div class="form-group">
+                                <label for="username">Username:</label>
+                                <input type="text" class="form-control" name="username" required>
+                            </div>
 
-        <label for="username">Username:</label>
-        <input type="text" name="username" required><br>
+                            <div class="form-group">
+                                <label for="first_name">First Name:</label>
+                                <input type="text" class="form-control" name="first_name" required>
+                            </div>
 
-        <label for="first_name">First Name:</label>
-        <input type="text" name="first_name" required><br>
+                            <div class="form-group">
+                                <label for="last_name">Last Name:</label>
+                                <input type="text" class="form-control" name="last_name" required>
+                            </div>
 
-        <label for="last_name">Last Name:</label>
-        <input type="text" name="last_name" required><br>
+                            <div class="form-group">
+                                <label for="password">Password:</label>
+                                <input type="password" class="form-control" name="password" required>
+                            </div>
 
-        <label for="password">Password:</label>
-        <input type="password" name="password" required><br>
+                            <button type="submit" class="btn btn-primary btn-block">Register</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-        <button type="submit">Register</button>
-    </form>
 </body>
 
 </html>
